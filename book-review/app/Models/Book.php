7593a40e98d58a44f366e61fe5c66a62b5785e81
Sebastian <shuertas@teknovate.biz-at-172302277217>
2024-04-01 15:45:45 -0600

    feat: queries in the laravel project, at book model

--- book-review/app/Models/Book.php
+++ book-review/app/Models/Book.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
@@ -12,5 +13,20 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
     }
 }
